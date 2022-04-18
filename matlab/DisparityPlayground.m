c8708c9dd2df0a12a598029931cd8541e1a176dd
QuoZer <m.d.panteleev@gmail.com>
2022-04-19 01:51:08 +0300

    new error method

--- matlab/DisparityPlayground.m
+++ matlab/DisparityPlayground.m
@@ -1,75 +1,99 @@
 close all;
 global SHOW BasePath Scale Model Models;
 
-RECALCULATE = false;
+RECALCULATE = true;
 SHOW = false;
-Model = "MEI";
-BasePath = "D:\Work\Coding\Repos\fisheye_stereo\data\All_Compar0.1m\"; %+ Model + "\";  %0.1m\
+BasePath = "D:\Work\Coding\Repos\fisheye_stereo\data\1_Compar0.1m\"; %+ Model + "\";  %0.1m\
 Scale = 10; 
-Models =["REG" "ATAN" "MEI" "SCARA" "KB"]
-distances = [1 2 3 4 5 6 7 8]; % 0.5m: 4 5 6 7.5 9 10 11 12 12.5 13 14 15  //  0.3m: 1 2 4 5 6 7.5 10 // 0.05m: 1 2 3 4 5 6 8 10 12
+Models =["REG" "ATAN" "MEI" "SCARA" "KB" "REAL_ATAN"]
+distances = [1 2 3 4 5 6 7 8 9 10]; % 0.5m: 4 5 6 7.5 9 10 11 12 12.5 13 14 15  //  0.3m: 1 2 4 5 6 7.5 10 // 0.05m: 1 2 3 4 5 6 8 10 12
 % MEI0.1: 1 2 3 4 5 6 7 8
+% ALL 1 2 3 4 5 6 7 8 9 10
 
-if (RECALCULATE)
-    regData = [];
-    reg_disp = [];    
-    reg_count = [];
-    fyData = []; %zeros(distances.size(), 
-    fy_disp = [];
-    fy_count = [];
-    GIGADATA = []
-    
+% diffImageAll(Models, 5)
+computeAllModels(Models, stereoParams01m540p, distances, RECALCULATE);
 
-    for cur_model = Models
-        fyData = [];
-        for dst = distances
-            %%%     FISHEYE        %%%
-            [fy_e, fy_disp, fy_mean, count] = computePlaneError(stereoParams01m, dst, cur_model);
-            fy_count = [fy_count count];
-            fyData = [fyData ; fy_e];
-            %%%     REGULAR        %%%
-%             [reg_e, reg_disp, reg_mean, count] = computePlaneError(stereoParams01m, dst, "REG");
-%             reg_count = [reg_count count]
-%             regData = [regData [reg_e; reg_disp; -reg_mean]];
-            disp("Distance  ready")
-        end
-        GIGADATA = [GIGADATA fyData] 
+
+%%%%%% FUNCTIONS %%%%%%
+
+
+function diffImageAll(modelDict, distance )
+    global BasePath
+    sample_path = BasePath + "REG/l_img_REG" + string(distance-1) + ".png";
+    sample_img = imread(sample_path);
+    sample_img = rgb2gray(sample_img);
+    for type = modelDict
+        imgLeft = BasePath + type + "/r_img_" + type + string(distance-1) + ".png";
+        lImage = imread(imgLeft);
+        
+        lImage = rgb2gray(lImage);
+        diffImage = lImage - sample_img;
+        
+%         pout_imadjust = imadjust(diffImage);
+%         pout_histeq = histeq(diffImage);
+        pout_adapthisteq = adapthisteq(diffImage);
+        
+%         montage({diffImage,pout_imadjust,pout_histeq,pout_adapthisteq},"Size",[1 4])
+%         title("Original Image and Enhanced Images using imadjust, histeq, and adapthisteq")
+        
+        figure('Name', 'Diff Image '+type);
+        imshow(pout_adapthisteq)
     end
-
 end
 
-figure;
-title("Models");
-xlabel('Distance, m');
-ylabel('MSE, m')
-plot(distances, GIGADATA, 'DisplayName',cur_model)
-hold on
-legend;
-% figure
-% plot(distances, [regData(1,:); fyData(1,:)]);
-% title(Model);
-% xlabel('Distance, m');
-% ylabel('MSE, m')
+function computeAllModels(modelDict, stereoParams, distances, recalculateFlag)
+    if (recalculateFlag)
+        fyData = []; %zeros(distances.size(), 
+        GIGADATA = [];
+        POINT_COUNT = [];
+        for cur_model = modelDict
+            disp(cur_model)
+            [fyData, counts] = computePlaneErrorDistances(distances, stereoParams, cur_model)
+            disp("Model ready")
+            GIGADATA = [GIGADATA fyData.'] 
+            POINT_COUNT = [POINT_COUNT counts.']
+        end
+    end
+    
+    createfigure(distances, POINT_COUNT);
+    createfigure(distances, GIGADATA);
+end
 
-% plot(distances, reg_count, distances, fy_count)
-% createfigure(distances, [regData; fyData])
-
+function [mses, counts] = computePlaneErrorDistances(distances, stereoParams, modelName)
+    mses = zeros(size(distances), 'double');    
+    counts = zeros(size(distances), 'double'); 
+    order = int16(1);
+    for dst = distances
+        %%%     FISHEYE        %%%
+        [fy_e, fy_disp, fy_mean, count] = computePlaneError(stereoParams, dst, modelName);
+%         fy_count = [fy_count count];
+        mses(int16(order)) =  fy_e;
+        counts(int16(order)) = count;
+        disp("Distance " + dst + " ready")
+        order = order +1;
+    end
+end
 
 function [MSE, D, M, Inds]  = computePlaneError(stereoParams, distance, type)
     global  BasePath;
     global SHOW;
     global Scale;
-    global Model;
     %base_path = BasePath + string(distance) + "m\";  % compar0.3m
 
-    targetDistance = -distance;
-    target_roi = [-0.4 0.6 -0.6 0.46 targetDistance/Scale-0.1 targetDistance/Scale+0.1];
+    targetDistance = distance;
+%     target_roi = [-0.4 0.6 -0.6 0.46 0.90*targetDistance/Scale 1.1*targetDistance/Scale];       % plane is a little shifted
+    k = distance / 0.7;
+    target_roi = [-0.4*k 0.6*k -0.6*k 0.46*k 0.50*targetDistance/Scale 1.5*targetDistance/Scale];
 
-    targetParamsVector = [0, 0, 1, targetDistance/Scale];   % normal + distance
+    targetParamsVector = [0, 0, 1, -targetDistance/Scale];   % normal + distance
     ref_model = planeModel(targetParamsVector);
 
-    imgRight = BasePath + type + "/l_img_"+ type + string(distance) + ".png";
-    imgLeft = BasePath + type + "/r_img_" + type + string(distance) + ".png";
+    imgLeft = BasePath + type + "/l_img_"+ type + string(distance-1) + ".png";
+    imgRight = BasePath + type + "/r_img_" + type + string(distance-1) + ".png";
+%     if (type~="REG")          % in older datasets L/R were mistaken                           yikes
+%         imgRight = BasePath + type + "/l_img_"+ type + string(distance-1) + ".png";
+%         imgLeft = BasePath + type + "/r_img_" + type + string(distance-1) + ".png";
+%     end
 
     lImage = imread(imgLeft);
     rImage = imread(imgRight);
@@ -111,11 +135,11 @@ function [MSE, D, M, Inds]  = computePlaneError(stereoParams, distance, type)
         plot(ref_model, 'color', 'white')
         hold off
         
-        figure;
-        imshow(disparityMapReg, [0, 64]);
-        title('Disparity Map'+string(targetDistance));
-        colormap jet
-        colorbar
+%         figure;
+%         imshow(disparityMapReg, [0, 64]);
+%         title('Disparity Map'+string(targetDistance));
+%         colormap jet
+%         colorbar
     end
 end
 
@@ -123,17 +147,32 @@ function [MSE, D] = findMSE(pt_cloud, plane_model)
     global Scale;
     error_sum = 0.0;
     squaredError_sum = 0.0;
+    ortho_method = false;
 %    figure;
 %     Animated_Plot = animatedline;
-    for elm = 1:pt_cloud.Count
-        pnt = pt_cloud.Location(elm,:);
-        
-        planePoint = [0, 0, plane_model.Parameters(4)];  % plane center
-        PQ = pnt - planePoint;
-        error = dot(PQ, plane_model.Normal)*Scale;     % difference [meters] 
+    z_dst = -plane_model.Parameters(4);
+    size = (10/Scale * z_dst / 0.7)/2;            % half side length 
+    edge_tan = size / z_dst;
 
-        error_sum = error_sum + error;
-        squaredError_sum = squaredError_sum + error^2;
+    for elm = 1:pt_cloud.Count
+        if (ortho_method)
+            pnt = pt_cloud.Location(elm,:);
+            planePoint = [0, 0, z_dst];  % plane center
+            PQ = pnt - planePoint;
+            error = dot(PQ, plane_model.Normal)*Scale;     % difference [meters] 
+            error_sum = error_sum + error;
+            squaredError_sum = squaredError_sum + error^2;
+        else
+            pnt = pt_cloud.Location(elm,:);
+            if (abs(pnt(1)/pnt(3)) < edge_tan || abs(pnt(2)/pnt(3)) < edge_tan)
+                planePoint = [0, 0, z_dst];  % plane center
+                pnt_dist = norm(pnt);
+                plane_dist = pnt_dist * z_dst / pnt(3);
+                error = (plane_dist - pnt_dist)*Scale;
+                error_sum = error_sum + error;
+                squaredError_sum = squaredError_sum + error^2;                
+            end
+        end
 
         
     end
@@ -148,65 +187,7 @@ function createfigure(X1, YMatrix1)
 %  X1:  vector of x data
 %  YMATRIX1:  matrix of y data
 
-% Create figure
-figure1 = figure;
-
-% Create axes
-axes1 = axes('Parent',figure1);
-hold(axes1,'on');
-
-% Create multiple lines using matrix input to plot
-YMatrix1;
-plot1  = errorbar(X1, YMatrix1(1,:), YMatrix1(2,:),'Marker','diamond',...
-    'LineStyle','--',...
-    'LineWidth',2 ); hold on;
-plot2 = errorbar(X1, YMatrix1(4,:), YMatrix1(5,:),'Marker','diamond',...
-    'LineStyle','--',...
-    'LineWidth',2 ); 
-
-
-xFit = linspace(0,50, 50);
-coefficients = polyfit(X1, YMatrix1(1,:), 1);
-yFit = polyval(coefficients , xFit);
-plot_l1 = plot(xFit, yFit, 'b-', 'LineWidth', 1); % Plot fitted line.
-
-coefficients = polyfit(X1, YMatrix1(4,:), 1);
-yFit = polyval(coefficients , xFit);
-plot_l2 = plot(xFit, yFit, 'r-', 'LineWidth', 1); % Plot fitted line.
-
-set(plot_l1,'Color',[0 0 1]);
-set(plot_l2,...
-    'Color',[0.850980392156863 0.325490196078431 0.098039215686274]);
-
-%plot1 = plot(X1,YMatrix1,'Marker','square');
-set(plot1(1),'DisplayName','"Традиционная" стереопара');
-set(plot2(1),'DisplayName','Предлагаемая стереосистема');
-
-% Create ylabel
-ylabel('Среднеквадратичная ошибка оценки  поверхности, м');
-
-% Create xlabel
-xlabel('Расстояние до поверхности, м');
-
-% Uncomment the following line to preserve the X-limits of the axes
- xlim(axes1,[-0.000209902368405811 16.0517554642163]);
-% Uncomment the following line to preserve the Y-limits of the axes
- ylim(axes1,[-3.8322756199846e-05 0.55351683918712]);
-box(axes1,'on');
-% Set the remaining axes properties
-set(axes1,'FontSize',14,'XGrid','on','YGrid','on');
-% Create legend
-legend1 = legend(axes1,'show');
-set(legend1,...
-    'Position',[0.172415836709068 0.839227796053986 0.262158950099176 0.0602310215285902]);
-end
-
-function createfigure2(X1, YMatrix1)
-%CREATEFIGURE(X1, YMatrix1)
-%  X1:  vector of x data
-%  YMATRIX1:  matrix of y data
-
-%  Auto-generated by MATLAB on 29-Mar-2022 00:21:43
+%  Auto-generated by MATLAB on 10-Apr-2022 23:59:03
 
 % Create figure
 figure1 = figure;
@@ -222,6 +203,7 @@ set(plot1(2),'DisplayName','ATAN','Marker','o','LineStyle','--');
 set(plot1(3),'DisplayName','MEI','Marker','square','LineStyle',':');
 set(plot1(4),'DisplayName','SCARA','Marker','diamond','LineStyle','-.');
 set(plot1(5),'DisplayName','KB','LineStyle','--');
+set(plot1(6),'DisplayName',['REAL',newline,'ATAN'],'LineWidth',3);
 
 % Create ylabel
 ylabel('MSE, m');
@@ -238,5 +220,6 @@ set(axes1,'XGrid','on','YGrid','on');
 % Create legend
 legend1 = legend(axes1,'show');
 set(legend1,...
-    'Position',[0.784930504754938 0.164294954721864 0.099000243842965 0.202242345838725]);
+    'Position',[0.776501766784452 0.145977011494253 0.0990065978549978 0.178347160479719]);
+
 end
